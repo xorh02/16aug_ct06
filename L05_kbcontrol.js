606a345c25acbbd270272081beefc5262fccd575
xorh02 <xavierong02@gmail.com>
2025-08-17 03:43:24 +0000

    Aug 17, 2025, 3:43 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,20 +1,23 @@
-let shapeColor ='blue';
-let circleSize =5;
+// let shapeColor ='blue';
+// let circleSize =5;
 
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    noStroke();
-}
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
+// }
 
-function mousePressed(){
-    shapeColor= color(random(255), random(255),random(255))
-    circleSize =5;
-}
+// function mousePressed(){
+//     shapeColor= color(random(255), random(255),random(255))
+//     circleSize =5;
+// }
 
 
-function mouseDragged(){
-    fill(shapeColor);
-    ellipse(mouseX,mouseY, circleSize,circleSize);
-    circleSize += 0.5;
-}
+// function mouseDragged(){
+//     fill(shapeColor);
+//     ellipse(mouseX,mouseY, circleSize,circleSize);
+//     circleSize += 0.5;
+// }
+
+
+//-------------------------------------------------------------------------------------------- 
