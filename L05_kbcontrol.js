b989d42b5d6d60e901a301cd13df818f383e19ce
xorh02 <xavierong02@gmail.com>
2025-08-17 07:47:11 +0000

    Aug 17, 2025, 7:47 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -22,57 +22,57 @@
 
 //-------------------------------------------------------------------------------------------- key pressed
 
-// let rectSize =50;
-
-// function setup(){
-//     createCanvas(400,400);
-// }
-
-// function draw(){
-//     background(220);
-
-//     let centerX = width/2;
-//     let centerY = height/2;
-
-//     let topLeftX = centerX -rectSize/2
-//     let topLeftY = centerY -rectSize/2
-
-//     rect(topLeftX , topLeftY , rectSize,rectSize)
-// }
-
-//-------------------------------------------------------------------------------------------- pressed & release random circles
+let rectSize =50;
 
 function setup(){
     createCanvas(400,400);
+}
+
+function draw(){
     background(220);
-    noStroke();
 
+    let centerX = width/2;
+    let centerY = height/2;
+
+    let topLeftX = centerX -rectSize/2
+    let topLeftY = centerY -rectSize/2
+
+    rect(topLeftX , topLeftY , rectSize,rectSize)
 }
 
-function keyPressed(){
-    let count = floor(random(5,21));
-    for(let i=0; i<count; i++){
-        // random size
-        let s = random(10,80);
-        //random position
-        let x = random(s/2,width -s/2);
-        let y = random(s/2,height -s/2);
+//-------------------------------------------------------------------------------------------- pressed & release random circles
 
-        //random fill
-        fill(random(255), random(255), random(255));
-        // create circle
-        ellipse(x , y   , s ,s)
-    }
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
 
-}
+// }
 
-function keyReleased(){
-    background(220);
-}
+// function keyPressed(){
+//     let count = floor(random(5,21));
+//     for(let i=0; i<count; i++){
+//         // random size
+//         let s = random(10,80);
+//         //random position
+//         let x = random(s/2,width -s/2);
+//         let y = random(s/2,height -s/2);
 
-let a= 5;
-let b = "5";
-let c = 7;
+//         //random fill
+//         fill(random(255), random(255), random(255));
+//         // create circle
+//         ellipse(x , y   , s ,s)
+//     }
 
-console.log(a===b);
+// }
+
+// function keyReleased(){
+//     background(220);
+// }
+
+// let a= 5;
+// let b = "5";
+// let c = 7;
+
+// console.log(a===b);
 
