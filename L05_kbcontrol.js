fd2eddced1dff3d1c02a2381c68121e9a88572c9
xorh02 <xavierong02@gmail.com>
2025-08-17 08:45:29 +0000

    Aug 17, 2025, 8:45 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -64,7 +64,7 @@ function keyPressed(){
     let count = floor(random(5,21));
 
     for(let i=0; i <count; i++){
-        
+        let size = random(10,80)
     }
 
     /* for (let i = 0; end ; step){
