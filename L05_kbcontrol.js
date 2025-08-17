64aa448c03c74e8617fb47de3797891f729eb39f
xorh02 <xavierong02@gmail.com>
2025-08-17 04:11:02 +0000

    Aug 17, 2025, 4:11 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -53,6 +53,7 @@ function keyPressed(){
     let count = floor(random(5,21));
     for(let i=0; i<count; i++){
         // random size
+        let s 
         //random position
         //random fill
         // create circle
